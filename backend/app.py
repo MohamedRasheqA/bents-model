e847ef56a9bd3937bff5f9fa50f1ca39387074e6
MohamedRasheqA <rasheq@klmsolutions.in>
2024-09-27 12:35:41 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -17,8 +17,13 @@ import langsmith
 
 from flask_cors import CORS
 
+
+
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://bents-model.vercel.app/"}})
+CORS(app, resources={r"/*": {"origins": [
+    "https://bents-model.vercel.app/",
+    "https://bents-model-4ppw.vercel.app/"
+]}})
 
 
 
