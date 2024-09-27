ae4c709841a0e8d56062c412464292637b6d97e7
rasheq <rasheq@klmsolutions.in>
2024-09-27 11:08:27 +0530

    messages

--- LLM-SERVER/app.py
+++ LLM-SERVER/app.py
@@ -18,7 +18,7 @@ import langsmith
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/*": {"origins": "https://bents-model.vercel.app/"}})
 
 
 
